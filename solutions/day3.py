ec67e58443254e9d08e2e58414d1e3907164a7a8
paulderaadt <paulderaadt@gmail.com>
2024-12-03 13:23:00 +0100

    solved

--- solutions/day3.py
+++ solutions/day3.py
@@ -1,6 +1,7 @@
 from utils.filereaders import Flatreader
 from re import findall, finditer
 
+
 def extract_mul(mul):
     match = findall(r'\d{1,3}', mul)
     match = list(map(int, match))
@@ -13,7 +14,6 @@ def part1(data):
     for mul in muls:
         mulpairs.append(extract_mul(mul))
     print(sum([a * b for a, b in mulpairs]))
-    return mulpairs
 
 
 def part2(data):
@@ -21,17 +21,18 @@ def part2(data):
     do = True
     multotal = 0
     for mul in muls:
-        if mul.group(0)[0:3] == 'mul':
+        if mul.group(0).startswith('mul'):
             mul = extract_mul(mul.group(0))
             if do:
                 a, b = mul
                 multotal += a * b
-        elif mul.group(0)[0:3] == 'do(':
+        elif mul.group(0).startswith('do('):
             do = True
-        elif mul.group(0)[0:3] == 'don':
+        elif mul.group(0).startswith('don'):
             do = False
-
     print(multotal)
+
+
 def main():
     data = Flatreader('../inputs/day3.txt').parse()
     data = data.replace('\n', '')
@@ -39,4 +40,5 @@ def main():
     part2(data)
 
 
-main()
+if __name__ == '__main__':
+    main()
